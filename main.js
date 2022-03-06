83e6549f0790673d3ed23b41732b1621863f0a1c
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-03-06 11:01:51 +0100

    náhodné umístění mince a panáčka po ploše

--- main.js
+++ main.js
@@ -5,5 +5,18 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 }
 */
 
+let mince = document.querySelector('#mince');
+let panacek = document.querySelector('#panacek');
 
-// sem začni psát svůj program
+// tady řeším NÁHODNÉ UMÍSTĚNÍ MINCE A PANÁČKA po celé ploše minus 36px (v/š mince), aby se mi neschovala
+var w = window.innerWidth;
+var h = window.innerHeight;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+mince.style.left = `${getRandomInt(0, w+1-36)}px`;
+mince.style.top = `${getRandomInt(0, h+1-36)}px`;
+panacek.style.left = `${getRandomInt(0, w+1-64)}px`;
+panacek.style.top = `${getRandomInt(0, h+1-70)}px`;
