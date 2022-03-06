2745fe9299df096aa3b8f1e4bb64f1047166ac7c
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-03-06 12:07:25 +0100

    doplnění omezení pohybu panáčka na viditelnou plochu

--- main.js
+++ main.js
@@ -11,35 +11,49 @@ let panacek = document.querySelector('#panacek');
 // tady řeším NÁHODNÉ UMÍSTĚNÍ MINCE A PANÁČKA po celé ploše minus 36px (v/š mince), aby se mi neschovala
 var w = window.innerWidth;
 var h = window.innerHeight;
+var panacekH = 70;
+var panacekW = 64;
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
-mince.style.left = `${getRandomInt(0, w+1-36)}px`;
-mince.style.top = `${getRandomInt(0, h+1-36)}px`;
-panacek.style.left = `${getRandomInt(0, w+1-64)}px`;
-panacek.style.top = `${getRandomInt(0, h+1-70)}px`;
+mince.style.left = `${getRandomInt(0, w + 1 - panacekW)}px`;
+mince.style.top = `${getRandomInt(0, h + 1 - panacekH)}px`;
+panacek.style.left = `${getRandomInt(0, w + 1 - panacekW)}px`;
+panacek.style.top = `${getRandomInt(0, h + 1 - panacekH)}px`;
 
-//tady řeším POHYB A ZMĚNA OBRÁZKU PANÁČKA při stisku šipek
+//tady řeším POHYB A ZMĚNA OBRÁZKU PANÁČKA při stisku šipek + omezení pohybu na viditelnou plochu
 function stiskKlavesy (event) {
   let klavesa = event.keyCode;
   console.log(klavesa);
   if (klavesa === 37) { //left
-    panacekPoziceHor = parseInt(panacek.style.left) -10;
-    panacek.style.left = `${panacekPoziceHor}px`;
     document.getElementById('panacek').src = `obrazky/panacek-vlevo.png`;
+    panacekPoziceHor = parseInt(panacek.style.left)
+    if (panacekPoziceHor > 0 + 10) {
+      panacekPoziceHor = panacekPoziceHor -10;
+      panacek.style.left = `${panacekPoziceHor}px`;
+    }
   } else if (klavesa === 38) { //up
-    panacekPoziceVer = parseInt(panacek.style.top) -10;
-    panacek.style.top = `${panacekPoziceVer}px`;
     document.getElementById('panacek').src = `obrazky/panacek-nahoru.png`;
+    panacekPoziceVer = parseInt(panacek.style.top)
+    if (panacekPoziceVer > 0 + 10) {
+      panacekPoziceVer = panacekPoziceVer -10;
+      panacek.style.top = `${panacekPoziceVer}px`;
+    }
   } else if (klavesa === 39) { //right
-    panacekPoziceHor = parseInt(panacek.style.left) +10;
-    panacek.style.left = `${panacekPoziceHor}px`;
     document.getElementById('panacek').src = `obrazky/panacek-vpravo.png`;
-  } else if (klavesa === 40) { //up
-    panacekPoziceVer = parseInt(panacek.style.top) +10;
-    panacek.style.top = `${panacekPoziceVer}px`;
+    panacekPoziceHor = parseInt(panacek.style.left)
+    if ((panacekPoziceHor + panacekW) < w - 10) {
+      panacekPoziceHor = panacekPoziceHor +10;
+      panacek.style.left = `${panacekPoziceHor}px`;
+    }
+  } else if (klavesa === 40) { //down
     document.getElementById('panacek').src = `obrazky/panacek.png`;
+    panacekPoziceVer = parseInt(panacek.style.top)
+    if ((panacekPoziceVer + panacekH) < h - 10) {
+      panacekPoziceVer = panacekPoziceVer +10;
+      panacek.style.top = `${panacekPoziceVer}px`;
+    }
   }
 }
